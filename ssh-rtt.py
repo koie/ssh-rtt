61d2ed82d76218bacbea826928594f0373cb5245
KOIE Hidetaka <hide@koie.org>
2019-05-31 17:37:02 +0900

    style-fix by flake8

--- ssh-rtt.py
+++ ssh-rtt.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python3.6
 
-import time
-import subprocess
-import random
 import argparse
+import random
 import statistics
+import subprocess
+import time
 
 ssh_cmd = "ssh"
+
 
 def ping(host, n):
     print(f"PING {host}")
     rtts = []
     args = [ssh_cmd, host, "cat"]
-    with subprocess.Popen(args=args, shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=0) as proc:
+    with subprocess.Popen(args=args,
+                          shell=False,
+                          stdin=subprocess.PIPE,
+                          stdout=subprocess.PIPE,
+                          bufsize=0) as proc:
         for i in range(n+1):
             start = time.clock_gettime(time.CLOCK_REALTIME)
-            nonce = (str(random.randint(10000000,99999999)) + "\n").encode()
+            nonce = (str(random.randint(10000000, 99999999)) + "\n").encode()
             proc.stdin.write(nonce)
             while True:
                 nonce2 = proc.stdout.readline()
@@ -31,9 +36,13 @@ def ping(host, n):
     std = statistics.stdev(rtts1, mean)
     print(f"host={host}: avg={mean*1000:.3f} ms, std={std*1000:.3f} ms")
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Ping over SSH")
-    parser.add_argument("hosts", metavar="HOST", nargs=argparse.REMAINDER, help="")
+    parser.add_argument("hosts",
+                        metavar="HOST",
+                        nargs=argparse.REMAINDER,
+                        help="")
     args = parser.parse_args()
     for host in args.hosts:
         ping(host, 10)
