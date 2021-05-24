cca694e92da7ed8dba9cac7aca8ae681fccbec40
KOIE Hidetaka <hide@koie.org>
2021-05-24 17:07:46 +0900

    avg and std are updated and showed for each line.

--- ssh-rtt.py
+++ ssh-rtt.py
@@ -1,21 +1,43 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/env python3
 
 import argparse
+import itertools
 import random
 import statistics
 import subprocess
 import sys
 import time
-import itertools
 
 ssh_cmd = "ssh"
 cat_cmd = "cat"
 
+class VarState:
+    # https://qpp.bitbucket.io/post/streaming_algorithm/
+    def __init__(self):
+        self.n = 0
+        self.pre = 0
+        self.cur = 0
+        self.m = 0
+        self.s = 0
+    def update(self, x):
+        self.n += 1
+        self.pre = self.cur
+        self.cur = x / self.n + (1 - 1.0 / self.n) * self.cur
+        self.m += (x - self.pre) * (x - self.cur)
+        self.s += x
+    def avg(self):
+        return self.s / self.n
+    def var(self):
+        return self.m / (self.n - 1) if self.n > 1 else 0
+    def stddev(self):
+        import math
+        return math.sqrt(self.var())
 
 def ping(ssh_arg, n, wait, size):
     hostname = " ".join(ssh_arg)
     print(f"PING {hostname}")
     rtts = []
+    vs = VarState()
     args = [ssh_cmd] + ssh_arg + [cat_cmd]
     with subprocess.Popen(args=args,
                           shell=False,
@@ -38,8 +60,15 @@ def ping(ssh_arg, n, wait, size):
                     continue
                 end = time.clock_gettime(time.CLOCK_REALTIME)
                 rtt = end - start
-                print(f"seq={i} time={rtt*1000:.3f} ms")
-                rtts.append(rtt)
+                if n > 0:
+                    rtts.append(rtt)
+                if i > 0:
+                    vs.update(rtt)
+                    mean = vs.avg()
+                    std = vs.stddev()
+                    print(f"seq={i} time={rtt*1000:.3f} ms, avg={mean*1000:.3f} ms, std={std*1000:.3f} ms")
+                else:
+                    print(f"seq={i} time={rtt*1000:.3f} ms")
                 break
     rtts1 = rtts[1:]
     if len(rtts1) > 0:
